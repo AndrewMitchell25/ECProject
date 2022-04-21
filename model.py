fcbaf1bc0f151a6446d51570e73e44db49f0ab50
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-04-21 17:21:13 -0400

    Dict of state data

--- model.py
+++ model.py
@@ -2,27 +2,38 @@ import requests
 import json
 import csv
 
-state_names = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "D.C.", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",  "Michigan", "Minnesota", "Mississippi", "Missouri",  "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",  "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-headers = {'Content-type': 'application/json'}
+stateNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "D.C.", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",  "Michigan", "Minnesota", "Mississippi", "Missouri",  "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",  "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
 stateData = {}
 
-stateID = ""
-for i in range(1,10):
-    stateID = ("LASST0" + str(i) + "0000000000003")
-    data = json.dumps({"seriesid": stateID,"startyear":"2011", "endyear":"2014"})
-    p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers)
+stateIDs = []
+state = 0
+for i in range(1,57):
+    x = "%02d" % (i,)
+    if(i == 7 or i==3 or i == 52 or i == 43 or i==14 or i==11):
+        continue
+    stateIDs.append("LASST" + x + "0000000000003")
     
-    stateData[state_names[i-1]] = "placeholder"
 
-for i in range(10,57):
-    stateID = ("LASST" + str(i) + "0000000000003")
-    data = json.dumps({"seriesid": stateID,"startyear":"2011", "endyear":"2014"})
-    p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers)
-    
-    stateData[state_names[i-1]] = "placeholder"
-
-
-
+startYear = 2003
+endYear = 2021
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": stateIDs,"startyear":str(startYear), "endyear":str(endYear), "annualaverage":"true","registrationkey":"345a69db035449398aa0dfb79dbe47f2"})
+p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers)
 json_data = json.loads(p.text)
-print(json_data)
+
+for i in range(50):
+    #get the current state in the data
+    currentState = json_data["Results"]["series"][i]["data"]
+    stateData[stateNames[i]] = list(range(endYear-startYear+1))
+    #for each year
+    for k in range(endYear-startYear+1):
+        #add up each month and divide by 12 to get average
+        yearlyAverage = 0
+        for j in range(12):
+            yearlyAverage += float(currentState[k*12+j]["value"])
+        yearlyAverage = yearlyAverage/12
+        yA = round(yearlyAverage, 3)
+        stateData[stateNames[i]][k] = yA
+print(stateData)
+
